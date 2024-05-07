4c66b32911da3cc31e2ee61a8c6d3de32b389a6b
phuongthuydo <lawdawg556nam1311@outlook.com>
2024-05-07 16:32:43 +0700

    commit

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-function minPathSum(grid) {
-  const m = grid.length;
-  const n = grid[0].length;
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  dp[0][0] = grid[0][0];
-  for (let i = 1; i < m; i++) {
-    dp[i][0] = dp[i - 1][0] + grid[i][0];
+function countSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    extendPalindrome(i, i);
+    extendPalindrome(i, i + 1);
   }
-  for (let j = 1; j < n; j++) {
-    dp[0][j] = dp[0][j - 1] + grid[0][j];
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+  return count;
+  function extendPalindrome(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
     }
   }
-  return dp[m - 1][n - 1];
 }
